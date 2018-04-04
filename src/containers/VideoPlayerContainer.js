e7ce63591e7ddf3008e299c1a92313c3d16530e3
zaid <zaidhusseini@gmail.com>
2018-04-03 17:36:22 -0700

    Passing all tests

--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,7 +1,20 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
+const mapStateToProp = (state) => {
+
+  return {
+    video: state.currentVideo
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    changeVideoPlayer: (video)=> dispatch(changeVideo(video))
+  }
+}
+
+var VideoPlayerContainer = connect(mapStateToProp, mapDispatchToProps)(VideoPlayer);
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
